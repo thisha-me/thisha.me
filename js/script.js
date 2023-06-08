c37d12069e770b75e3900c05e7633ed90ce92d37
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-06-08 12:27:04 +0530

    new version

---
+++ js/script.js
@@ -0,0 +1,82 @@
+//----------------------toggle icon nav bar----------------------------
+let menuIcon=document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// --------------------Scroll section active link-----------------------
+let sections=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top=window.scrollY;
+        let offest=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top>=offest && top<offest+height){
+            navLinks.forEach(links=>{
+                links.classList.remove("active");
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // -------------------------sticky navbar--------------------------
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //-------remove toggle icon and navbar when click navbar link (scroll)------
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+// -------------------------Scroll Reveal--------------------------
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:250
+ });
+
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
+
+// -------------------------typed js--------------------------
+const typed=new Typed('.multiple-text',{
+    strings:['NFT Developer','Digital Creator', 'SE Undergraduate' ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+//------------------------------------cursor--------------------------
+const cursor = document.querySelector('.cursor');
+var timeout;
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px; display:block;")
+    function mouseStopped(){
+        cursor.style.display="none";
+    }
+    clearTimeout(timeout);
+    timeout=setTimeout(mouseStopped,1000);
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
